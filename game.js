b74fed9a6653033017d424f305f9de7359df82aa
Sadak Halil <s.halil@yahoo.com>
2021-07-12 16:57:34 +0200

    finalized the game

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
 // function for random computerPlay
+let roundResult;
+let playerScore = 0;
+let computerScore = 0;
+
 function computerSelection() {
     let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random()*3)];
@@ -13,58 +17,43 @@ function playerSelection () {
 
 function playRound (player, computer){
     if (player === computer) {
-        console.log(`draw, player`, player);
-        console.log(`draw, computer`, computer);
-        let result = "draw";
-        console.log("Draw!");
-        return result;
+        roundResult = "draw";
+        return roundResult;  
     }
     else {
         switch (player) {
             case `rock`:
-                computer === `scissors` ? console.log(`You Win!`) : console.log(`You Loose :(`);
-                console.log(`rock case player`, player);
-                console.log(`rock case computer`, computer);
-                break;
+                computer === `scissors` ? roundResult = "win" : roundResult = "loose";
+                return roundResult;
             case `paper`:
-                computer === `rock` ? console.log(`You Win!`) : console.log(`You Loose :(`);
-                console.log(`paper case player`, player);
-                console.log(`paper case computer`, computer);
-                break;
+                computer === `rock` ? roundResult = "win" : roundResult = "loose";
+                return roundResult;
             case `scissors`:
-                computer === `paper` ? console.log(`You Win!`) : console.log(`You Loose :(`);
-                console.log(`scissors case player`, player);
-                console.log(`scissors case computer`, computer);
-                break;
+                computer === `paper` ? roundResult = "win" : roundResult = "loose";
+                return roundResult;
         }
     }
 }
-playRound(playerSelection(),computerSelection())
 
-/*let playerScore = 0;
-let computerScore = 0;
-while (playerScore !=3 || computerScore !=3){
+while (playerScore !== 3 && computerScore !== 3){
     switch (playRound(playerSelection(),computerSelection())) {
-        case `You win!`:
+        case `win`:
+            playerScore += 1;
+            console.log(`You won this round! Score is You(`,playerScore,`) : Computer (`,computerScore,`)`);
+            break;
+        case `loose`:
+            computerScore += 1
+            console.log(`You lost the round. Score is You(`, playerScore, `) : Computer (`, computerScore,`)`);
+            break;
+        case `draw`:
+            console.log(`Draw round! Score is still You(`, playerScore, `) : Computer (`, computerScore,`)`);
+            break;
     }
-}*/
-
-
-/*
-
-draw - 3 options compare strings with ===
-paper - rock
-paper - scissors
-rock - paper
-rock - scissors
-scissors - rock
-scissors - paper
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-*/
+if (computerScore === 3) {
+    console.log ('You lost the game!');
+}
+else{
+    console.log('You won the game!')
+}
